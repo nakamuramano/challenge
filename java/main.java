934717e5954fec30ad68906409fa5d75457e97c3
nakamura_arisa <nakamura.arisa918@gmail.com>
2022-11-29 12:47:06 +0000

    add

---
+++ java/main.java
@@ -0,0 +1,60 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello Java");
+  }
+}
+
+class Main{
+  public static void main(String[] args){
+    System.out.println("Hello World");
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("こんにちは、Java");
+    
+    //この文章をコメントにしてください
+    
+    // 「こんにちは、Java」と出力してください
+    
+    
+  }
+}
+
+class Main{
+  public static void main(String[] args){
+     この文章をコメントしてください
+     
+     // 「こんにちは、java」と出力してください
+  }
+  
+}
+
+class Main {
+  public static void main(String[] args) {
+    // 数値の17を出力してください
+    System.out.println(17);
+    
+    // 5に3を足した値を出力してください
+    System.out.println(5 + 3);
+    
+    // 「5 + 3」を文字列として出力してください
+    System.out.println("5 + 3");
+    
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    // 12を3で割った値を出力してください
+    System.out.println(12 / 3);
+    
+    // 3に6を掛けた値を出力してください
+    System.out.println(3 * 6);
+    
+    // 8を3で割った時の余りを出力してください
+    System.out.println(8 % 3);
+    
+  }
+}
