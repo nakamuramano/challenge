8c6d789a4302ad9138f281860fb523adbd0b2f6a
nakamura_arisa <nakamura.arisa918@gmail.com>
2022-12-01 13:44:08 +0000

    add

--- java/main.java
+++ java/main.java
@@ -95,3 +95,77 @@ class Main {
     
   }
 }
+
+class Main {
+  public static void main(String[] args) {
+    int number1 = 3;
+    
+    // int型の変数number2を定義し、7を代入してください
+    int number2 = 7;
+    
+    // number1 * number2を出力してください
+    System.out.println(number1 * number2);
+    
+    // 変数textに「プログラミングを勉強しよう」を代入してください
+    String text ="プログラミングを勉強しよう";
+    
+    // 「Progateで」と変数textを連結して出力してください
+    System.out.println("Progateで" + text);
+    
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    int number = 11;
+    String text = "Ruby";
+    System.out.println(number);
+    System.out.println(text);
+    
+    // 変数numberを9で上書きしてください
+    number = 9;
+    
+    // 変数numberを出力してください
+    System.out.println(number);
+    
+    // 変数textを「Java」で上書きしてください
+    text = "Java"; 
+    
+    // 変数textを出力してください
+    System.out.println(text);
+    
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    int number = 3;
+    System.out.println(number);
+    
+    // 変数numberの値に7を足して、変数numberを上書きしてください
+    number = number + 7;
+    
+    // 変数numberを出力してください
+    System.out.println(number);
+      
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    int number = 8;
+    
+    // 変数numberに7をかけて、変数numberを上書きしてください
+    number *= 7;
+    
+    // 変数numberを出力してください
+    System.out.println(number);
+    
+    // 変数numberの値に1を足して、変数numberを上書きしてください
+    number ++;
+    
+    // 変数numberを出力してください
+    System.out.println(number);
+    
+  }
+}
