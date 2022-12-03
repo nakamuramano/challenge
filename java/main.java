731bf155239c6a60c035fa8d024b9ac55e698794
nakamura_arisa <nakamura.arisa918@gmail.com>
2022-12-03 14:30:06 +0000

    add

--- java/main.java
+++ java/main.java
@@ -239,3 +239,112 @@ class Main {
     
   }
 }
+
+class Main {
+  public static void main(String[] args) {
+    // 変数nameに、好きな名前を代入してください
+    String name = "hanako";
+    
+    // 変数ageに、好きな年齢を代入してください
+    int age = 17;
+    
+    // 変数heightに、好きな身長を代入してください（例: 1.2）
+    double height = 1.4;
+    
+    // 変数weightに、好きな体重を代入してください（例: 20.0）
+    double weight = 25.0;
+    
+    // 変数bmiに、BMIを計算して代入してください
+    double bmi = 25.0 / 1.4 / 1.4;
+    
+    // 名前、年齢、身長、体重、BMIに関する情報を出力してください
+    System.out.println("名前は" + name + "です");
+    System.out.println("年齢は" + age + "歳です");
+    System.out.println("身長は" + height + "mです");
+    System.out.println("体重は" + weight + "kgです");
+    System.out.println("BMIは" + bmi + "です");
+    
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    // 「true」を用いて、「真」を表す真偽値を出力してください
+    System.out.println(true);
+    
+    // 「false」を用いて、「偽」を表す真偽値を出力してください
+    System.out.println(false);
+    
+    // 「==」を用いて、値を比較した結果を出力してください
+    System.out.println(12/4 == 3);
+    
+    // 「!=」を用いて、値を比較した結果を出力してください
+    System.out.println(12/4 != 3);
+    
+    // 変数を定義し、値を比較した結果を代入してください
+    boolean bool = 3*9 == 27;
+    
+    // 変数boolの値を出力してください
+    System.out.println(bool);
+    
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    // 「4 + 2」と「6」を比較し、falseとなるようにしてください
+    System.out.println(4 + 2 < 6);
+    
+    // 「4 + 2」と「6」を比較し、trueとなるようにしてください
+    System.out.println(4 + 2 <= 6);
+    
+  }
+}
+
+
+class Main {
+  public static void main(String[] args) {
+    // trueと出力されるようにしてください
+    System.out.println(true || false);
+    
+    // falseと出力されるようにしてください
+    System.out.println(false && true);
+    
+    // 「8 < 5」かつ「3 >= 2」の結果を出力してください
+    System.out.println(8 < 5 && 3 >= 2);
+    
+    // 「8 < 5」または「3 >= 2」の結果を出力してください
+    System.out.println(8 < 5 || 3 >= 2 );
+    
+    // 「8 < 5」でない、の結果を出力してください
+    System.out.println(!(8 < 5));
+    
+  }
+}
+
+
+class Main {
+  public static void main(String[] args) {
+    // 条件式に直接trueをいれてください
+    if (true) {
+      System.out.println("条件式がtrueのため、出力されます");
+    }
+    
+    // 条件式に直接falseをいれてください
+    if (false) {
+      System.out.println("条件式がfalseのため、出力されません");
+    }
+    
+    int x = 5;
+    // if文を用いて、変数xが2より大きいとき「xは2より大きい」と出力してください
+    if (x > 2) {
+      System.out.println("xは2より大きい");
+    }
+    
+    // if文を用いて、変数xが8より大きいとき「xは8より大きい」と出力してください
+    if (x > 8) {
+      System.out.println("xは8より大きい");
+    }
+    
+  }
+}
