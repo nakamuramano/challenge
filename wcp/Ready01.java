af3a6aad2543875267ca06fdcc4894a6484f4af1
nakamura_arisa <nakamura.arisa918@gmail.com>
2022-12-07 13:48:48 +0000

    add

--- wcp/Ready01.java
+++ wcp/Ready01.java
@@ -3,3 +3,42 @@ public class Ready01 {
     System.out.println("Hello World!");
   }
 }
+
+public class Chapter02 {
+	public static void main(String[] args) {
+		System.out.println("Hell WEBCAMP！");
+		System.out.println("Hello Java!");
+	}
+}
+
+public class Ready02 {
+  public static void main (String [] args) {
+	  System.out.println("Hello World!");
+	  
+	  
+		int num1;
+		String str1;
+		num1 = 10;
+		str1 = "Hello World!";
+		System.out.println(num1);
+		System.out.println(str1);
+
+		num1 = 20;
+		System.out.println(num1);
+
+		int num2 = 30;
+		System.out.println(num2);
+
+  }
+}
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 20;
+		String str = "花子";
+		System.out.println(str);
+		str = "太郎";
+		System.out.println(str);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
