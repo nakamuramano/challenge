5fd4205a5412020f4164c102aae26ff895d2f955
nakamura_arisa <nakamura.arisa918@gmail.com>
2022-12-07 06:52:41 +0000

    add

--- java/main.java
+++ java/main.java
@@ -10,23 +10,13 @@ class Main{
   }
 }
 
-class Main {
-  public static void main(String[] args) {
-    System.out.println("こんにちは、Java");
-
-    //この文章をコメントにしてください
-
-    // 「こんにちは、Java」と出力してください
-
-
-  }
-}
 
 class Main{
   public static void main(String[] args){
-     この文章をコメントしてください
+     //この文章をコメントしてください
 
      // 「こんにちは、java」と出力してください
+     System.out.println("こんにちはjava");
   }
 
 }
@@ -403,7 +393,7 @@ class Main {
 class Main {
   public static void main(String[] args) {
     int number = 10;
-    
+
     // while文を用いて、numberが0より大きい場合に繰り返す、繰り返し処理を作ってください
     while (number > 0) {
       System.out.println(number);
@@ -418,7 +408,7 @@ class Main {
     for (int i = 1; i <= 10; i ++){
       System.out.println(i + "回目のループです");
     }
-    
+
   }
 }
 
@@ -433,7 +423,7 @@ class Main {
       System.out.println(i);
       i++;
     }
-    
+
     System.out.println("=== for文 ===");
     for (int j = 1; j < 10; j++) {
       // jが3の倍数のとき、処理をスキップしてください
@@ -448,13 +438,13 @@ class Main {
   public static void main(String[] args) {
     // 変数namesに、配列を代入してください
     String[] names = {"にんじゃわんこ","ひつじ仙人","ベイビーわんこ"};
-    
+
     // インデックス番号が0の要素を出力してください
     System.out.println(names[0]);
-    
+
     // インデックス番号が2の要素を出力してください
     System.out.println(names[2]);
-    
+
   }
 }
 
@@ -462,40 +452,40 @@ class Main {
   public static void main(String[] args) {
     // 変数languagesを定義し、配列を代入してください
     String [] languages = {"Ruby", "PHP", "Python"};
-    
+
     // インデックス番号が「1」の要素を出力してください
     System.out.println(languages[1]);
-    
+
     // インデックス番号が「1」の要素を「Java」で上書きしてください
     languages [1] = "Java";
-    
+
     // インデックス番号が「1」の要素を出力してください
     System.out.println(languages[1]);
-    
+
   }
 }
 
 class Main {
   public static void main(String[] args) {
     String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
-    
+
     // for文を用いて、「私の名前は◯◯です」と出力してください
     for (int i = 0; i < names.length; i++) {
       System.out.println("私の名前は" + names[i] + "です");
     }
-    
+
   }
 }
 
 class Main {
   public static void main(String[] args) {
     String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
-    
+
     // 配列用のfor文を用いて、「私の名前は◯◯です」と出力してください
     for (String name: names) {
       System.out.println("私の名前は" + name + "です" );
     }
-    
+
   }
 }
 
@@ -503,10 +493,10 @@ class Main {
   public static void main(String[] args) {
     // 変数numbersに、与えられた数字の配列を代入してください
     int [] numbers = {1, 4, 6, 9, 13, 16};
-    
+
     int oddSum = 0;
     int evenSum = 0;
-    
+
     // for文を用いて、配列numbersの偶数の和と奇数の和を求めてください
     for (int number: numbers) {
       if (number % 2 == 0) {
@@ -526,7 +516,7 @@ class Main {
   public static void main(String[] args) {
     hello();
   }
-  
+
   public static void hello() {
     // "Hello World"を、"Hello Java"に書き換えてください
     System.out.println("Hello Java");
@@ -537,31 +527,31 @@ class Main {
   public static void main(String[] args) {
     // printDataメソッドを呼び出してください
     printData ();
-    
+
   }
-  
+
   // printDataメソッドを定義してください
  public static void printData() {
     System.out.println("私の名前はKate Jonesです");
  }
-  
+
 }
 
 class Main {
   public static void main(String[] args) {
     // 引数に「Kate Jones」を渡してください
     printData("Kate Jones");
-    
+
     // 引数に「John Christopher Smith」を渡してください
     printData("John Christopher Smith");
-    
+
   }
 
   // 引数を受け取るようにしてください
   public static void printData(String name) {
     // 「私の名前は◯◯です」と出力されるように書き換えてください
     System.out.println("私の名前は" + name + "です");
-    
+
   }
 }
 
@@ -577,6 +567,6 @@ class Main {
     System.out.println("私の名前は" + name + "です");
     // 「年齢は◯◯歳です」と出力してください
     System.out.println("年齢は" + age + "歳です");
-    
+
   }
 }
