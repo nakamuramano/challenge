9700358d6d3f9aad9a46b4a1d6c3d4d432c1c426
nakamura_arisa <nakamura.arisa918@gmail.com>
2022-12-10 11:59:12 +0000

    add

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset = "utf-8">
+    <title>php</title>
+    <link rel = "stylesheet" href= "stylesheet.css">
+  </head>
+  <body>
+    <?php
+      // 'Hello, world!'をechoしてください
+      echo 'Hello, world!';
+    ?>
+  
+    <br>
+
+    <?php
+      // 7 * 2をechoしてください
+      echo 7 * 2;
+    ?>
+
+    <br>
+
+    <?php
+      // 8 % 3をechoしてください
+      echo 8 % 3;
+    ?>
+    
+    <!-- この下で5 + 7を出力しましょう -->
+    <?php
+      echo 5 + 7;
+    ?>
+
+    <br>
+
+    <!-- この下で'5 + 7'を出力しましょう -->
+    <?php
+      echo '5 + 7';
+    ?>
+    
+    <!-- この下に変数$fruitを書いていきましょう！ -->
+    <?php
+      $fruit = 'りんご';
+      echo $fruit;
+    ?>
+
+    <br>
+
+    <!-- この下に変数$sumを書いていきましょう！ -->
+    <?php
+      $sum = 8 + 9;
+      echo $sum;
+    ?>
+    
+    <?php
+
+      $x = 5;
+      $y = 2;
+      $a = 8;
+      $b = 4;
+    
+    ?>
+
+    <!-- この下で$xの計算をしていきましょう -->
+    <?php
+      $x += 10;
+      echo $x;
+    ?>
+
+    <br>
+
+    <!-- この下で$yの計算をしていきましょう -->
+    <?php
+      $y *= 5;
+      echo $y;
+    ?>
+
+    <br>
+
+    <!-- この下で$aの計算をしていきましょう -->
+    <?php
+      $a++;
+      echo $a;
+    ?>
+
+    <br>
+  
+    <!-- この下で$bの計算をしていきましょう -->
+    <?php
+      $b--;
+      echo $b;
+    ?>
+    
+  </body>
+</html>
